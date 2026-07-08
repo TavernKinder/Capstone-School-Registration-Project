e4b89048cf98d64846a3fb07da1c40b71eed2afa
ParkR15MineR <parkerlamb@gmail.com>
2026-07-08 07:35:20 -0600

    last minute changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Sidebar from './Sidebar.jsx'
 
 import App from './App.jsx'
 import Profile from './Profile.jsx'
